3383a574f59841f4a4cb5a1da82868a7c59f3a35
Caroll Carvalho <xccaroll@hotmail.com>
2024-03-15 05:25:55 -0300

    -Updated index.html with new icon and correct name -imported components from Diego -installed new plugins based on new components -updated main page with new components: Header + Tabs -Add Table Component in the page

--- README.md
+++ README.md
@@ -4,9 +4,21 @@ Project created with UI plus functions as filters, pagination and sorting.
 Using technologies: React, React Query, TailwindCSS e shadcn/ui
 
 To install tailwind dependence:
-pnpm install -D tailwindcss postcss autoprefixer
+=> pnpm install -D tailwindcss postcss autoprefixer
+
 After, to initialize the tailwind.config.js file:
-pnpm dlx tailwindcss init -p
+=> pnpm dlx tailwindcss init -p
 PS: We use tag "-p" to create a create postcss.config.js file, because for tailwind works with vite is obligate has this file.
 
-To execute: pnpm run dev
+To execute:
+=> pnpm run dev
+
+To Icons we use Lucide:
+=> pnpm i lucide-react
+Link: https://lucide.dev/icons/
+
+Some components use dependencies, to install:
+=> pnpm i tailwind-variants tailwind-merge
+
+To Select Component install:
+=> pnpm i @radix-ui/react-select
--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,70 @@
+import { Plus, Search, FileDown, MoreHorizontal } from 'lucide-react';
+import { Header } from './components/header';
+import { Tabs } from './components/tabs';
+import { Button } from './components/ui/button';
+import { Control, Input } from './components/ui/input';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from './components/ui/table';
+
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <div className="py-10 space-y-8">
+      <div>
+        <Header />
+        <Tabs />
+      </div>
+      <main className="max-w-6xl mx-auto space-y-5">
+        <div className='flex items-center gap-3'>
+          <h1 className="text-xl font-bold">Tags</h1>
+          <Button variant='primary'>
+            <Plus className='size-3' />
+            Create
+          </Button>
+        </div>
+        <div className="flex items-center justify-between">
+          <Input variant='filter'>
+            <Search className='size-3' />
+            <Control placeholder='Search tags..' />
+          </Input>
+          <Button>
+            <FileDown className='size-3' />
+            Export
+          </Button>
+        </div>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead></TableHead>
+              <TableHead>Tag</TableHead>
+              <TableHead>Amount of videos</TableHead>
+              <TableHead></TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {/* JS to create a array to repeat fake data */}
+            {Array.from({ length: 10 }).map((_value, index) => {
+              return (
+                <TableRow key={index}>
+                  <TableCell></TableCell>
+                  <TableCell>
+                    <div className='flex flex-col gap-0.5'>
+                      <span className='font-medium'>React</span>
+                      <span className='text-xs text-zinc-500 uppercase'>0ebaac40-b14e-4b34-8ca6-fb9140153e89</span>
+                    </div>
+                  </TableCell>
+                  <TableCell className='text-zinc-300'>
+                    13 video(s)
+                  </TableCell>
+                  <TableCell className='text-right'>
+                    <Button size='icon'>
+                      <MoreHorizontal className='size-4' />
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+      </main>
+    </div>
   )
 }
