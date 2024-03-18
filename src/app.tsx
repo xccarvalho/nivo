5032b6afb0fadba8d43c663e831b0cc312a71879
Caroll Carvalho <xccaroll@hotmail.com>
2024-03-18 00:53:27 -0300

    -created filter function -created debounce hook to delay

--- src/app.tsx
+++ src/app.tsx
@@ -1,55 +1,81 @@
-import { Plus, Search, FileDown, MoreHorizontal } from 'lucide-react';
-import { Header } from './components/header';
-import { Tabs } from './components/tabs';
-import { Button } from './components/ui/button';
-import { Control, Input } from './components/ui/input';
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from './components/ui/table';
-import { Pagination } from './components/pagination';
-import { keepPreviousData, useQuery } from '@tanstack/react-query'
-import { useSearchParams } from 'react-router-dom';
+import { Plus, Search, FileDown, MoreHorizontal, Filter } from "lucide-react";
+import { Header } from "./components/header";
+import { Tabs } from "./components/tabs";
+import { Button } from "./components/ui/button";
+import { Control, Input } from "./components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./components/ui/table";
+import { Pagination } from "./components/pagination";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "react-router-dom";
+import { useState } from "react";
+// import useDebounceValue from "./hooks/use-debounce-value";
 
 export interface TagResponse {
-  first: number
-  prev: number | null
-  next: number
-  last: number
-  pages: number
-  items: number
-  data: Tag[]
+  first: number;
+  prev: number | null;
+  next: number;
+  last: number;
+  pages: number;
+  items: number;
+  data: Tag[];
 }
 
 export interface Tag {
-  title: string
-  slug: string
-  amountOfVideos: number
-  id: string
+  title: string;
+  slug: string;
+  amountOfVideos: number;
+  id: string;
 }
 
-
 export function App() {
-  const [searchParams] = useSearchParams();
-  const page = searchParams.get('page') ? Number(searchParams.get('page')) : 1;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const urlFilter = searchParams.get("filter") ?? "";
+  const [filter, setFilter] = useState(urlFilter);
+
+  // to make a delay - not used now, because app is short
+  // const debouncedFilter = useDebounceValue(filter, 1000);
+
+  const page = searchParams.get("page") ? Number(searchParams.get("page")) : 1;
 
   const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
-    // The cash is stored by key ('get-tags') and each time that browsing a different page is created a cache for each list ('get-tags')
-    queryKey: ['get-tags', page],
+    // The cache is stored by key ('get-tags') and each time a different page is loaded, a cache is created for each list ('get-tags')
+    // TO USE debouncedFilter => queryKey: ["get-tags", debouncedFilter, page],
+    queryKey: ["get-tags", urlFilter, page],
     queryFn: async () => {
-      const response = await fetch(`http://localhost:3333/tags?_page=${page}&_per_page=10`)
-      const data = await response.json()
+      const response = await fetch(
+        // // TO USE debouncedFilter => `http://localhost:3333/tags?_page=${page}&_per_page=10&title=${debouncedFilter}`
+        `http://localhost:3333/tags?_page=${page}&_per_page=10&title=${urlFilter}`
+      );
+      const data = await response.json();
 
-      // give me a 2s delay
-      await new Promise(resolve => setTimeout(resolve, 2000))
+      // gives me a 2s delay - this was changed by Debounced hook - but I left it to see the cache working
+      await new Promise((resolve) => setTimeout(resolve, 2000));
 
-      return data
+      return data;
     },
     // to not "blink" the screen content
     placeholderData: keepPreviousData,
-    // Set how much time cash stay stored - It is in ms(milliseconds)
-    staleTime: 1000 * 60
-  })
+    // Defines the time cache is stored - It is done in ms(milliseconds)
+    staleTime: 1000 * 60,
+  });
+
+  function handleFilter() {
+    setSearchParams((params) => {
+      params.set("page", "1");
+      params.set("filter", filter);
+      return params;
+    });
+  }
 
   if (isLoading) {
-    return null
+    return null;
   }
 
   return (
@@ -59,20 +85,30 @@ export function App() {
         <Tabs />
       </div>
       <main className="max-w-6xl mx-auto space-y-5">
-        <div className='flex items-center gap-3'>
+        <div className="flex items-center gap-3">
           <h1 className="text-xl font-bold">Tags</h1>
-          <Button variant='primary'>
-            <Plus className='size-3' />
+          <Button variant="primary">
+            <Plus className="size-3" />
             Create
           </Button>
         </div>
         <div className="flex items-center justify-between">
-          <Input variant='filter'>
-            <Search className='size-3' />
-            <Control placeholder='Search tags..' />
-          </Input>
+          <div className="flex items-center gap-1">
+            <Input variant="filter">
+              <Search className="size-3" />
+              <Control
+                placeholder="Search tags.."
+                onChange={(e) => setFilter(e.target.value)}
+                value={filter}
+              />
+            </Input>
+            <Button onClick={handleFilter} className="rounded-full">
+              <Filter className="size-3" />
+              Filter
+            </Button>
+          </div>
           <Button>
-            <FileDown className='size-3' />
+            <FileDown className="size-3" />
             Export
           </Button>
         </div>
@@ -93,28 +129,35 @@ export function App() {
                 <TableRow key={tag.id}>
                   <TableCell></TableCell>
                   <TableCell>
-                    <div className='flex flex-col gap-0.5'>
-                      <span className='font-medium'>{tag.title}</span>
-                      <span className='text-xs text-zinc-500 uppercase'>{tag.id}</span>
+                    <div className="flex flex-col gap-0.5">
+                      <span className="font-medium">{tag.title}</span>
+                      <span className="text-xs text-zinc-500 uppercase">
+                        {tag.id}
+                      </span>
                     </div>
                   </TableCell>
-                  <TableCell className='text-zinc-300'>
-                    {tag.amountOfVideos} {tag.amountOfVideos > 1 ? 'videos' : 'video'}
+                  <TableCell className="text-zinc-300">
+                    {tag.amountOfVideos}{" "}
+                    {tag.amountOfVideos > 1 ? "videos" : "video"}
                   </TableCell>
-                  <TableCell className='text-right'>
-                    <Button size='icon'>
-                      <MoreHorizontal className='size-4' />
+                  <TableCell className="text-right">
+                    <Button size="icon">
+                      <MoreHorizontal className="size-4" />
                     </Button>
                   </TableCell>
                 </TableRow>
-              )
+              );
             })}
           </TableBody>
         </Table>
-        {tagsResponse &&
-          <Pagination pages={tagsResponse.pages} items={tagsResponse.items} page={page} />
-        }
+        {tagsResponse && (
+          <Pagination
+            pages={tagsResponse.pages}
+            items={tagsResponse.items}
+            page={page}
+          />
+        )}
       </main>
     </div>
-  )
+  );
 }
---
+++ src/hooks/use-debounce-value.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+/* 
+a hook function Debounce is used to create a delay, or execute another actions.
+For example: When you have a input and the user is typing so you need take an action with the information that's being typed, but only after the user stop to typing
+So Debounce stay watching and when the user STOP to typing, he execute the action inside his function, in this case is a Delay.
+*/
+export default function useDebounceValue<T = unknown>(value: T, delay: number) {
+  const [debouncedValue, setDebouncedValue] = useState(value)
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value)
+    }, delay)
+    return () => {
+      clearTimeout(handler)
+    }
+  }, [value, delay])
+  
+  return debouncedValue
+}
